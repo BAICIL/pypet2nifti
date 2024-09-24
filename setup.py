f8fd8637e73f62df489d2d82442c6fee2b40d8d9
Dhruman Goradia <Dhruman.Goradia2@bannerhealth.com>
2024-09-24 08:07:17 -0700

    updated

--- setup.py
+++ setup.py
@@ -1,17 +1,36 @@
 from setuptools import setup, find_packages
-
+# Full setup configuration
 setup(
-    name='image_converter_package',
-    version='0.1',
-    packages=find_packages(),
+    name='pupet2nifti',  
+    version='0.1',  
+    description='A tool to convert DICOM and ECAT images to NIfTI format.',  
+    author='Dhruman Goradia, PhD', 
+    author_email='Dhruman.Goradia2@bannerhealth.com',  
+    url='https://github.com/BAICIL/pypet2nifti',
+    
+    # Automatically find and include all packages in the project
+    packages=find_packages(),  
+
+    # List dependencies (install_requires can also directly list dependencies if requirements.txt is not used)
     install_requires=[
         'pydicom',
         'nibabel'
         'dcm2niix'
     ],
+
+    # Entry point configuration
     entry_points={
         'console_scripts': [
-            'pet-to-bids=cli:main',
+            'convert-to-nifti=cli:main',  # Links the CLI script 'convert-to-nifti' to the 'main' function in cli.py
         ],
     },
+
+    # Additional metadata
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    
+    python_requires='>=3.9',  # Specifies that Python 3.9 or newer is required
 )
