c48ae54391f5c0684da92a4fcfb901c3ad182e1c
Dhruman Goradia <Dhruman.Goradia2@bannerhealth.com>
2024-09-26 01:41:39 -0700

    corrected required arguments

--- pypet2nifti/pet_to_bids.py
+++ pypet2nifti/pet_to_bids.py
@@ -1,5 +1,5 @@
 import argparse
-from image_converter import Converter  
+from .image_converter import Converter  
 
 def main():
     """
@@ -8,8 +8,8 @@ def main():
     Uses argparse to capture command-line inputs and initialize the Converter class with the provided arguments.
     """
     parser = argparse.ArgumentParser(description="Convert DICOM or ECAT images to NIfTI format with sidecar json file.")
-    parser.add_argument('--source_data', type=str, help='Path to the source DICOM or ECAT data.')
-    parser.add_argument('--destination_folder', type=str, help='Destination folder for the converted NIfTI file.')
+    parser.add_argument('--source_data', type=str, help='Path to the source DICOM or ECAT data.', required=True)
+    parser.add_argument('--destination_folder', type=str, help='Destination folder for the converted NIfTI file.', required=True)
     parser.add_argument('--subject_id', type=str, default=None, help='Subject ID for the output file naming.')
     parser.add_argument('--session_id', type=str, default=None, help='Session ID for the output file naming.')
     parser.add_argument('--tracer', type=str, default=None, help='Tracer information for the output file naming.')
